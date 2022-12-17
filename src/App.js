b3dfc314600f1ea99d4501868e7d50cda65b1aaa
SEB_FE_41_김소라 <110882351+kimsorara@users.noreply.github.com>
2022-12-17 12:07:45 +0900

    첫번째 파일

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Todo from './todo.js';
+import Home from './home';
+import Nav from './nav';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  const todos = useSelector((state) => state.todos.todo);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Nav data={todos}></Nav>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          {todos?.map((el) => (
+            <Route
+              key={el.id}
+              path={`/todos/${el.id}`}
+              element={<Todo value={el} />}
+            ></Route>
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/toolkit/reducer.js
@@ -0,0 +1,35 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initialState = {
+  todo: [
+    {
+      id: "2f383fa8",
+      day: "12월",
+      title: "툴킷 기능구현",
+      content: "아어려웡....",
+    },
+  ],
+};
+
+export const todoList = createSlice({
+  name: "todoList",
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      state.todo.push(action.payload);
+    },
+
+    remove: (state, action) => {
+      state.todo = state.todo.filter((el) => el.id !== action.payload);
+    },
+    path: (state, action) => {
+      state.todo = state.todo.map((todo) =>
+        todo.id === action.payload.id ? (todo = action.payload) : todo
+      );
+    },
+  },
+});
+console.log(todoList.reducer);
+//store에서 add, remove, complte 액션을 내보낸다.
+export default todoList.reducer;
+export const { add, remove, path } = todoList.actions;
